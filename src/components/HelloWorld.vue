2361717182e4ff42ca37d96e57f06d728dcf11da
Vvoox <khalil.daoulat@gmail.com>
2020-03-02 15:34:07 +0100

    add v-for reDirective

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,45 @@
+<!--<template>-->
+<!--&lt;!&ndash;  <div class="hello">&ndash;&gt;-->
+<!--&lt;!&ndash;    <h1>{{ msg }}</h1>&ndash;&gt;-->
+<!--&lt;!&ndash;    <p>&ndash;&gt;-->
+<!--&lt;!&ndash;      For a guide and recipes on how to configure / customize this project,<br>&ndash;&gt;-->
+<!--&lt;!&ndash;      check out the&ndash;&gt;-->
+<!--&lt;!&ndash;      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.&ndash;&gt;-->
+<!--&lt;!&ndash;    </p>&ndash;&gt;-->
+<!--&lt;!&ndash;    <h3>Installed CLI Plugins</h3>&ndash;&gt;-->
+<!--&lt;!&ndash;    <ul>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;    </ul>&ndash;&gt;-->
+<!--&lt;!&ndash;    <h3>Essential Links</h3>&ndash;&gt;-->
+<!--&lt;!&ndash;    <ul>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;    </ul>&ndash;&gt;-->
+<!--&lt;!&ndash;    <h3>Ecosystem</h3>&ndash;&gt;-->
+<!--&lt;!&ndash;    <ul>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>&ndash;&gt;-->
+<!--&lt;!&ndash;    </ul>&ndash;&gt;-->
+<!--&lt;!&ndash;  </div>&ndash;&gt;-->
+<!--</template>-->
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="app-5">
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
+</div>
+
 </template>
 
+
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
